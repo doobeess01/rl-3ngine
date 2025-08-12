9d107e4018eabd4c52e8ef0e3786872d485a0447
David Dee <david.m.dee01@gmail.com>
2025-08-11 19:04:08 -0500

    Implemented entity rendering priority

--- game/rendering.py
+++ game/rendering.py
@@ -6,7 +6,7 @@ import g
 
 from game.fov import update_fov
 from game.components import Position, Graphic, Tiles, MapShape, VisibleTiles, MemoryTiles
-from game.tags import IsGhost
+from game.tags import IsGhost, IsItem, IsCreature
 from game.tiles import TILES
 from game.message_log import MessageLog
 from game.text import Text
@@ -28,17 +28,30 @@ def render_map(map_: tcod.ecs.Entity, screen_shape: tuple[int, int], center: tup
 
     g.console.rgb[console_slices] = TILES["graphic"][np.where(visible, light_tiles, dark_tiles)]
 
+    rendered_priority: dict[Position, int] = {}
     for e in g.registry.Q.all_of(tags=[map_], components=[Position, Graphic]):
         pos = e.components[Position]
         rendered_pos = pos - (camera[1], camera[0])
         graphic = e.components[Graphic]
+
+        render_order = 1
+        if IsItem in e.tags:
+            render_order = 2
+        if IsCreature in e.tags:
+            render_order = 3
+        if g.player == e:
+            render_order = 4
+        if rendered_priority.get(rendered_pos, 0) >= render_order:
+            continue  # Do not render over a more important entity
+        rendered_priority[rendered_pos] = render_order
 
         in_bounds = 0 <= rendered_pos.x < screen_shape[1] and 0 <= rendered_pos.y < screen_shape[0]
 
         rendered = False
         if in_bounds:
             offset = (0 if camera[1] > 0 else camera[1], 0 if camera[0] > 0 else camera[0])
-            rendered = visible[(rendered_pos+offset).ij] != (IsGhost in e.tags)
+
+            rendered = visible[(rendered_pos+offset).ij] != (IsGhost in e.tags) 
 
         if in_bounds and rendered:
             g.console.rgb[["ch", "fg"]][rendered_pos.ij] = graphic.ch, graphic.fg
